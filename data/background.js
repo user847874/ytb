b1c1b2309e325c3556b6c29880b2bb57df6d837a
user847874 <vladimir84787@gmail.com>
2025-08-06 13:33:06 +0300

    Delete data/background.js

--- data/background.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  fetch('https://ytb.infy.uk/data/data.json')
-    .then(res => {
-    return res.text();
-  }).then(res => {
-    sendResponse(res);
-  })
-  return true
-});
